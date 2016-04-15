e52366b5639dd34009ba1b97b1b9e571fede8bab
silverlaurelin <silverlaurelin@gmail.com>
2016-04-15 13:27:43 +0300

    dev

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,14 +2,18 @@ from model.group import Group
 
 def test_modify_group_name(app):
    old_groups = app.group.get_groups_list()
-   app.group.modify_first_group(Group(name="new name"))
+   group = Group(name="new name")
+   group.id = old_groups[0].id
+   app.group.modify_first_group(group)
    new_groups = app.group.get_groups_list()
    assert len(old_groups) == len(new_groups)
+   old_groups[0] = group
+   assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_group_header(app):
-    old_groups = app.group.get_groups_list()
-    app.group.modify_first_group(Group(header="new header"))
-    new_groups = app.group.get_groups_list()
-    assert len(old_groups) == len(new_groups)
+#def test_modify_group_header(app):
+#   old_groups = app.group.get_groups_list()
+#   app.group.modify_first_group(Group(header="new header"))
+#   new_groups = app.group.get_groups_list()
+#   assert len(old_groups) == len(new_groups)
 
     
