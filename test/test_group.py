2e39208b72cadd6a00e97809982e7fb8c75a656c
silverlaurelin <silverlaurelin@gmail.com>
2016-04-15 07:18:02 +0300

    list

--- test/test_group.py
+++ test/test_group.py
@@ -3,8 +3,10 @@ from model.group import Group
 
 def test_add_group(app):
      success = True
+     old_groups = app.group.get_groups_list()
      app.group.create(Group(name="some name", header="some logo", footer="some footer"))
-
+     new_groups = app.group.get_groups_list()
+     assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
     success = True
