54f17ae7d9149a9564c361ccf0bbac9e869e6360
silverlaurelin <silverlaurelin@gmail.com>
2016-04-15 17:06:31 +0300

    dev

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,21 +5,21 @@ def test_add_group(app):
      old_groups = app.group.get_groups_list()
      group = Group(name="some name", header="some header", footer="some footer")
      app.group.create(group)
+     assert len(old_groups) + 1 == app.group.count()
      new_groups = app.group.get_groups_list()
-     assert len(old_groups) + 1 == len(new_groups)
      old_groups.append(group)
      assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_groups_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_groups_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_groups_list()
+#   group = Group(name="", header="", footer="")
+#   app.group.create(group)
+#   new_groups = app.group.get_groups_list()
+#   assert len(old_groups) + 1 == len(new_groups)
+#   old_groups.append(group)
+#   assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
