0da80e858b74153d2480854e5e9d532b76754abb
silverlaurelin <silverlaurelin@gmail.com>
2016-04-15 07:24:59 +0300

    groups list comparing

--- test/test_group.py
+++ test/test_group.py
@@ -10,7 +10,10 @@ def test_add_group(app):
 
 def test_add_empty_group(app):
     success = True
+    old_groups = app.group.get_groups_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,10 +1,15 @@
 from model.group import Group
 
 def test_modify_group_name(app):
+   old_groups = app.group.get_groups_list()
    app.group.modify_first_group(Group(name="new name"))
-   app.session.logout()
+   new_groups = app.group.get_groups_list()
+   assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_groups_list()
     app.group.modify_first_group(Group(header="new header"))
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) == len(new_groups)
 
     
